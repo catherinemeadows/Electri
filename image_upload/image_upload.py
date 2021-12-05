15786c610196b71bbe39e72f0409ec613e454cb7
EthanBaron14 <ethanbaron@gwu.edu>
2021-12-05 15:01:54 -0500

    Created S3 delete API

--- image_upload/image_upload.py
+++ image_upload/image_upload.py
@@ -3,6 +3,8 @@ import boto3
 import os
 from botocore.exceptions import ClientError
 
+logging.basicConfig(filename='image_upload.log', level=logging.DEBUG)
+
 def upload_image(file_name, bucket, object_name=None):
     # Upload file to S3 bucket
     if object_name is None:
@@ -30,11 +32,26 @@ def download_image(object_name, bucket, file_name=None):
     # Attempt to download an image from S3
     try:
         response = s3.download_file(bucket, object_name, file_name)
-    except ClientError as e :
+    except ClientError as e:
         logging.error(e)
         return False
     return True
 
+def delete_image(object_name, bucket):
+    # Delete file from S3 bucket
+    # Create client object
+    s3 = boto3.client('s3')
+
+    # Attempt to delete an image from S3
+    try:
+        response = s3.delete_object(Bucket = bucket, Key = object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 # Upload a sample image
-upload_image('image_samples/sample1.jpg', 'senior-design-images', 'upload.jpg')
-download_image('cars/sample1.jpg', 'senior-design-images', 'image_samples/download.jpg')
+upload_image('image_samples/upload.jpg', 'senior-design-images', 'upload.jpg')
+download_image('cars/upload.jpg', 'senior-design-images', 'image_samples/download.jpg')
+delete_image('cars/upload.jpg', 'senior-design-images')
