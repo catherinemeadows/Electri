78625e4dd667f7010efe84434f32760cf00e4f1b
EthanBaron14 <ethanbaron@gwu.edu>
2021-12-05 14:46:42 -0500

    Created download API

--- image_upload/image_upload.py
+++ image_upload/image_upload.py
@@ -4,7 +4,7 @@ import os
 from botocore.exceptions import ClientError
 
 def upload_image(file_name, bucket, object_name=None):
-    # Upload file to S3 bucket "senior-design-images"
+    # Upload file to S3 bucket
     if object_name is None:
         object_name = os.path.basename(file_name)
 
@@ -19,5 +19,22 @@ def upload_image(file_name, bucket, object_name=None):
         return False
     return True
 
+def download_image(object_name, bucket, file_name=None):
+    # Download file from S3 bucket
+    if file_name is None:
+        file_name = object_name
+    
+    # Create client object
+    s3 = boto3.client('s3')
+
+    # Attempt to download an image from S3
+    try:
+        response = s3.download_file(bucket, object_name, file_name)
+    except ClientError as e :
+        logging.error(e)
+        return False
+    return True
+
 # Upload a sample image
-upload_image('image_samples/sample1.jpg', 'senior-design-images', 'sample1.jpg')
+upload_image('image_samples/sample1.jpg', 'senior-design-images', 'upload.jpg')
+download_image('cars/sample1.jpg', 'senior-design-images', 'image_samples/download.jpg')
