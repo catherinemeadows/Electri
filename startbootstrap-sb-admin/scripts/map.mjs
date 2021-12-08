1604f95ec410fbe53b3d0a07e5be98636eb9fe51
Nathaniel Bury <nbury@gwu.edu>
2021-12-07 19:58:32 -0500

    front end set up

--- startbootstrap-sb-admin/scripts/map.mjs
+++ startbootstrap-sb-admin/scripts/map.mjs
@@ -1,10 +1,50 @@
 // Import required AWS SDK clients and commands for Node.js
 import { QueryCommand } from "@aws-sdk/client-dynamodb";
 import { ddbClient } from "../libs/ddbClient.mjs";
-import { writeFile} from "fs"
+import { writeFile, existsSync} from "fs"
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";// Helper function that creates Amazon S3 service client module.
+const REGION = "us-east-1"; //e.g. "us-east-1"
+// Create an Amazon S3 service client object.
+const s3Client = new S3Client({ region: REGION });
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function download_image(path) {
+  const bucketParams = {
+    Bucket: "senior-design-images",
+    Key: path,
+  };
+  try {
+    // Create a helper function to convert a ReadableStream to a string.
+    const streamToString = (stream) =>
+      new Promise((resolve, reject) => {
+        const chunks = [];
+        stream.on("data", (chunk) => chunks.push(chunk));
+        stream.on("error", reject);
+        stream.on("end", () => resolve(Buffer.concat(chunks)));
+      });
+
+    // Get the object} from the Amazon S3 bucket. It is returned as a ReadableStream.
+    const data = await s3Client.send(new GetObjectCommand(bucketParams));
+    // Convert the ReadableStream to a string.
+    const bodyContents = await streamToString(data.Body);
+    writeFile("dist/assets/"+path, bodyContents,err3 => 
+    {
+        if (err3) {
+        console.error(err3)
+        return
+        }
+    })
+    return 1
+  } catch (err) {
+    console.log("Error", err);
+    return 0
+  }
+}
+
 
 // Set the parameters
 export const params = {
@@ -36,7 +76,16 @@ export const run = async () => {
       }
       json_data['matches'].push(match)
       const content = JSON.stringify(json_data);
-
+      try {
+        if (!existsSync("dist/assets/"+element.img_path.S)){
+          console.log("downloading asset from " + element.img_path.S)
+          const k = download_image(element.img_path.S)
+        }
+      } catch (err) {
+        console.log("Error", err);
+        //const k = download_image(element.img_path.S)
+      }
+      
       writeFile('dist/test.json', content, err3 => 
       {
           if (err3) {
@@ -53,7 +102,7 @@ export const run = async () => {
 };
 while (1) {
   run();
-  await sleep(2000);
+  await sleep(10000);
 }
            
 
