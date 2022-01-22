afaf74692092dc05d24b55e3b6eaac7a544266ba
Nathaniel Bury <nbury@gwu.edu>
2022-01-22 15:22:15 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE alerts (
     vehicle_year int (4),
     color varchar(20),
     color_rgb blob,
-    PRIMARY KEY(alert_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE image_info (
@@ -44,15 +44,15 @@ CREATE TABLE image_info (
     license_plate varchar(32),
     latitude decimal (6,4),
     longitude decimal (6,4),
-    PRIMARY KEY(img_id)
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE matches (
     match_id int(8) not null auto_increment,
     img_id int(8) not null,
     alert_id int(8) not null,
-    FOREIGN KEY (img_id) REFERENCES image_info(img_id),
-    FOREIGN KEY (alert_id) REFERENCES alerts(alert_id),
+    FOREIGN KEY (img_id) REFERENCES image_info(id),
+    FOREIGN KEY (alert_id) REFERENCES alerts(id),
     PRIMARY KEY(match_id)
 );
 
@@ -62,17 +62,17 @@ values ("admin", "admin", "Admin", "User", "admin_fake@email.com", "DC MPD");
 
 -- ALERTS
 -- alerts with matches in db: 
-insert into alerts(alert_id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
+insert into alerts(id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
 values (00010000, 1, "Washington, DC", "DC", 38.9072, 77.0639, "FN9173", "Toyota", "Camry", 2010, "Black");
 
-insert into alerts(alert_id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
+insert into alerts(id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
 values (00010001, 1, "Cincinnati", "OH", 39.1031, 84.5120, "AA00AA", "Ford", "Bronco", 2016, "Blue");
 
-insert into alerts(alert_id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
+insert into alerts(id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
 values (00010002, 1, "Boston", "MA", 42.3601, 71.0589, "215BG2", "Honda", "Civic", 2017, "White"); 
 
 -- alerts with no matches: 
-insert into alerts(alert_id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
+insert into alerts(id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color)
 values (00010003, 0, "Phoenix", "AZ", 33.4484, 112.0704, "BY8567", "Ford", "F-150", 2018, "Red"); 
 
 -- insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
