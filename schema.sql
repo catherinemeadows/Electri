a55ad49cff5774c405bbd2f3771694ecea7b9d59
Catherine Meadows <meadowsc@gwu.edu>
2022-01-22 14:01:31 -0500

    update with sample data

--- schema.sql
+++ schema.sql
@@ -22,6 +22,8 @@ CREATE TABLE alerts (
     alert_status int(1) not null,
     city varchar(32),
     alert_state varchar(32),
+    latitude decimal (6,4),
+    longitude decimal (6,4),
     license_plate varchar(8),
     make varchar(32),
     model varchar(32),
@@ -50,3 +52,34 @@ CREATE TABLE matches (
     FOREIGN KEY (alert_id) REFERENCES alerts(alert_id),
     PRIMARY KEY(match_id)
 );
+
+-- USERS
+insert into user(username, user_password, fname, lname, email, organization)
+values ("admin", "admin", "Admin", "User", "admin_fake@email.com", "DC MPD");
+
+-- ALERTS
+-- alerts with matches in db: 
+insert into alerts(alert_id, alert_status, city, alert_state, latitude, longitude, license_plate, make, model, vehicle_year, color, color_rgb)
+values (00010000, 1, "Washington, DC", "DC", 38.9072, 77.0639, "FN9173", "Toyota", "Camry", 2010, "Black");
+
+insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+values (00010001, 1, "Cincinnati", "OH", 39.1031, 84.5120, "AA00AA", "Ford", "Bronco", 2016, "Blue");
+
+insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+values (00010002, 1, "Boston", "MA", 42.3601, 71.0589, "215BG2", "Honda", "Civic", 2017, "White"); 
+
+-- alerts with no matches: 
+insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+values (00010003, 0, "Phoenix", "AZ", 33.4484, 112.0704, "BY8567", "Ford", "F-150", 2018, "Red"); 
+
+-- insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+-- values (00010004, 0, )
+
+-- insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+-- values (00010005, 0, )
+
+-- insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+-- values (00010005, 0, )
+
+-- insert into alerts(alert_id, alert_status, city, alert_state, license_plate, make, model, vehicle_year, color, color_rgb)
+-- values (00010007, 0, )
